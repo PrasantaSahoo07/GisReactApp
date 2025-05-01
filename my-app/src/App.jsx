e71a7179dd08da724a2ef6a1b2e0e9f20d9a72cb
PrasantaSahoo07 <prasanta07official@gmail.com>
2025-05-01 09:50:10 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 import React from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/sparc-logo.svg'
+
 import './App.css'
 import MapComponent from './Module/MapComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
